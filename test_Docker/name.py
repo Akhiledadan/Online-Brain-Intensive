7af15a355234f92f932b41f666c86bd09e0e6db9
akhi <akhilddn5@gmail.com>
2017-09-01 09:36:47 +0200

    testing the docker files

---
+++ test_Docker/name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python2.7 -tt
+
+import sys
+
+def hello(name):
+  if name == 'akhil' or name == 'akhi':
+   print name, 'edadan'
+  else:
+   print 'unknown'
+
+def main():
+  hello(sys.argv[1])
+
+if __name__ == '__main__':
+  main()
+
+
+
